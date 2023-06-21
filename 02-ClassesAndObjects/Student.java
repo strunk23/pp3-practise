2fdc23b8fd62cd45ecd76002dba860e185dd169a
strunk23 <miloszwiktor58>
2023-06-21 09:49:07 +0200

    Initial files

---
+++ 02-ClassesAndObjects/Student.java
@@ -0,0 +1,8 @@
+public class Student {
+    public static void main(String[] args) {
+        String name = "Steve";
+        byte age = 25;
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
